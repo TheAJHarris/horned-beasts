a534acddf4b788983106f29a18cc3f5cfb2885f4
Anthony Johnson <theanthonyjohnson@gmail.com>
2021-09-23 16:32:37 -0500

    finished lab 04

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
 import rawData from './data.json';
+import SelectedBeast from './SelectedBeast';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       allBeasts: rawData,
+      selectedBeast: {},
+      showModal: false,
     }
+  }
+  handleClose = () => {
+    this.setState({
+      showModal: false,
+    })
+  }
+  displayModal = (nameOfBeast) => {
+    const selectedBeast = rawData.find(beast => beast.title === nameOfBeast);
+    this.setState({
+      selectedBeast: selectedBeast,
+      showModal: true,
+    })
+  }
+  displayFilteredBeasts= (allBeasts) => {
+    this.setState ({
+      allBeasts: allBeasts,
+    })
   }
 
   render() {
@@ -18,7 +38,16 @@ class App extends React.Component {
       <div className="App">
 
         <Header />
-        <Main allBeasts={this.state.allBeasts}/>
+        <Main 
+          allBeasts={this.state.allBeasts} 
+          displayModal= {this.displayModal}
+          displayFilteredBeasts= {this.displayFilteredBeasts}
+        />
+        <SelectedBeast 
+          selectedBeast={this.state.selectedBeast} 
+          show={this.state.showModal}
+          handleClose={this.handleClose}
+        />
         <Footer />
       </div>
     );
